ca3357a817df803e84c96bb646a0c7b4aa7580e5
vanHalen <gordIRL@users.noreply.github.com>
2017-07-28 10:22:05 +0100

    01 JumpStart Project

---
+++ resources/js/main.js
@@ -0,0 +1,27 @@
+ //alert("It has worked")
+
+
+$(document).ready(function(){
+
+  alert("hello");
+
+
+
+
+
+
+/*
+    $('.container').css('background-color', 'red');
+    */
+
+
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 4000,
+      slidesToShow: 1,
+      arrows:false,
+      draggable:false,
+      pauseOnHover:false
+     });
+
+});
